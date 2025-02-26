b8df3db4a40b8228db7cd761fde462ea00923288
Jaeheon Shim <git@jaeheonshim.com>
2025-02-25 19:37:51 -0500

    Use tensors for indices_map and partition_roots

--- wrapper/inference.py
+++ wrapper/inference.py
@@ -17,22 +17,29 @@ class AtomicPartitionInference:
         ### Partitioning
         desired_partitions = 20
         neighborhood_distance = 4
-        partitions, extended_partitions = part_graph_extended(G, desired_partitions, neighborhood_distance)
+        partition_set, extended_partition_set = part_graph_extended(G, desired_partitions, neighborhood_distance)
 
-        num_partitions = len(partitions)
+        num_partitions = len(partition_set)
 
         partitioned_atoms = []
-        indices_map = [] # Table mapping each atom in each partition back to its index in the original atoms object
 
-        for part in extended_partitions:
+        indices_map = [] # Table mapping each atom in each partition back to its index in the original atoms object
+        partition_roots = [] # Roots of each partition (True if root, False if not)
+
+        for i, part in enumerate(extended_partition_set):
             current_partition = []
             current_indices_map = []
+
             for atom_index in part:
                 current_partition.append(atoms[atom_index])
-                current_indices_map.append(atoms[atom_index].index)
+                current_indices_map.append(atom_index)
 
             partitioned_atoms.append(ase.Atoms(current_partition, cell=atoms.cell, pbc=atoms.pbc)) # It's important to pass atoms.cell and atoms.pbc here
             indices_map.append(current_indices_map)
+            partition_roots.append([j in partition_set[i] for j in current_indices_map])
+
+        indices_map = torch.tensor(indices_map)
+        partition_roots = torch.tensor(partition_roots)
 
         ### Graph Regressor
         all_embeddings = torch.zeros((len(atoms), self.model_adapter.embedding_size), dtype=torch.float32, device=self.model_adapter.device)
@@ -43,10 +50,7 @@ class AtomicPartitionInference:
             with torch.no_grad():
                 part_embeddings = self.model_adapter.forward_graph(input_graph)
 
-            for j, node in enumerate(part):
-                original_index = indices_map[i][j]
-                if original_index in partitions[i]: # If the node is a root node of the partition
-                    all_embeddings[original_index] = part_embeddings[j]
+            all_embeddings[indices_map[i][partition_roots[i]]] = part_embeddings[partition_roots[i]]
 
         ### Extract Energy
         with torch.no_grad():
